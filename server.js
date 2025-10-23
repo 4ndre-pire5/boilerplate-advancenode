1fc5cd51ae26de7c293f264d64b855894092a61a
Andre Pires <erdnapfaserip@gmail.com>
2025-10-23 12:01:29 -0300

    test: A new test

--- server.js
+++ server.js
@@ -7,16 +7,16 @@ const fccTesting = require('./freeCodeCamp/fcctesting.js');
 const app = express();
 
 fccTesting(app); //For FCC testing purposes
+
+app.set('view engine', 'pug');
+app.set('views', './views/pug');
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.route('/').get((req, res) => {
-  res.render('index');
+  res.render('index', { title: 'Hello', message: 'Please login' });
 });
-
-app.set('view engine', 'pug');
-app.set('views', './views/pug');
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
