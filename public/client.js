bb5882169c8eb81918ed6a2be611ffef527ec52d
Andre Pires <erdnapfaserip@gmail.com>
2025-10-27 12:39:28 -0300

    feat: Challenge 21

--- public/client.js
+++ public/client.js
@@ -8,9 +8,15 @@ $(document).ready(function () {
       (data.connected ? 'hasjoined the chat.' : 'has left the chat.');
     $('#messages').append($('<li>').html('<b>' + message + '<b>'));
   });
+
+  socket.on('chat message', data => {
+    console.log('socket.on 1');
+    $('messages').append($('<li>').text(`${data.username}: ${data.message}`));
+  });
   
   $('form').submit(function () {
       var messageToSend = $('#m').val();
+      socket.emit('chat message', messageToSend);
       $('#m').val('');
       return false;
   });
